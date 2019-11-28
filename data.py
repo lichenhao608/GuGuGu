fa32d9aca2da147ddbeb804b9a0ad02c6d9f4d16
Chenhao Li <chenhal5@uci.edu>
2019-11-27 19:43:29 -0800

    Update data.py
    have Dataset object for loading Dataloader for better memory usage

--- data.py
+++ data.py
@@ -1,18 +1,143 @@
+# -------------------------------------------------------------------------- #
+# Follow the tutorial of creating Dataloader of PyTorch                      #
+# Website: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html #
+# -------------------------------------------------------------------------- #
+
+from __future__ import print_function, division
+import os
+import torch
+from skimage import io, transform
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+import warnings
+warnings.filterwarnings("ignore")
+
+train_path = 'data/train/'
+label_file = 'data/train_labels.csv'
+
+
+class CellImgDataset(Dataset):
+    '''Cell image dataset'''
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        '''
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the training images.
+            transform (callable, optional): Optional transform to be applied on
+                a sample.
+        '''
+        self.cellimg_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.cellimg_frame)
+
+    def __getitem__(self, index):
+        if torch.is_tensor(index):
+            index = index.tolist()
+
+        img_name = os.path.join(
+            self.root_dir, self.cellimg_frame.iloc[index, 0])
+        image = io.imread(img_name)
+        label = self.cellimg_frame.iloc[index, 1]
+        sample = {'image': image, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
+class Rescale(object):
+    '''Rescale the image in a sample to a given size'''
+
+    def __init__(self, output_size):
+        '''
+        Args:
+            output_size (tuple or int): Desired output size. If tuple, output is
+                matched to output_size. If int, smaller of image edges is matched to
+                output_size keeping aspect ratio the same.
+        '''
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+
+        return {'image': img, 'label': label}
+
+
+class RandomCrop(object):
+    '''Crop randomly the image in a sample'''
+
+    def __init__(self, output_size):
+        '''
+        Args:
+            output_size (tuple or int): Desired output size. If int, square crop
+                is made.
+        '''
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top:(top + new_h), left:(left + new_w)]
+
+        return {'image': img, 'label': label}
+
+
+class ToTensor(object):
+    '''Convert ndarrays in sample to Tensors.'''
+
+    def __call__(self, sample):
+        image, label = sample['image'], sample['label']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'label': torch.from_numpy(label)}
 
 
 def load_data(label_file, img_dir, num=None):
-    '''
-    Convert image file into numpy array
+    '''Convert image file into numpy array
 
     Args:
         label_file: file directory to the csv file containing label info
         img_dir: repository containing images
         num: number of file to load, if None, all files will be loaded, defualt
             None
-
     Return:
         images: (N, 96, 96) shape image file
         y: (N,) shape label info
