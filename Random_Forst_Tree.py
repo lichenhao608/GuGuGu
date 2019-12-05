bfb40478d5a9c6cf52b7023c9670a569bc62bfc4
Chenhao Li <chenhal5@uci.edu>
2019-12-04 22:36:00 -0800

    result after 20 epochs

--- Random_Forst_Tree.py
+++ Random_Forst_Tree.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from data import load_data
+from projutils import load_data
 
 
 if __name__ == "__main__":
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         effect_area, y, train_size=0.8)
 
     model.fit(train, y_train)
-    model.score(test, y_test)
+    print(model.score(test, y_test))
